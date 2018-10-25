68bc1296894ed1c65c6e1b9338b665d77af1b641
何欣 <shrong.he@gmail.com>
2018-10-25 20:13:09 +0800

    mpeg define

--- svs_mu/svs_mu_stream/inc/svs_adapter_mpeg_def.h
+++ svs_mu/svs_mu_stream/inc/svs_adapter_mpeg_def.h
@@ -65,8 +65,8 @@ typedef struct
 
 typedef enum _enES_TYPE_E
 {
-    ES_TYPE_VIDEO,       // ��������ES
-    ES_TYPE_AUDIO,       // ��������ES
+    ES_TYPE_VIDEO,
+    ES_TYPE_AUDIO,
 
     ES_TYPE_MAX
 }ES_TYPE_E;
@@ -173,6 +173,141 @@ typedef enum _enES_TYPE_E
 #define SDT_TID   0x42
 
 #define MPEG_BUFFER_SIZE (64*TS_PACKET_SIZE)
+
+#define H264_START_CODE 4
+#define H265_START_CODE 4
+
+
+#define PS_HEAD_BUF_SIZE_MAX   32
+#pragma pack(1)
+union littel_endian_size
+{
+    unsigned short int  length;
+    unsigned char       byte[2];
+};
+struct pack_start_code
+{
+    unsigned char start_code[3];
+    unsigned char stream_id[1];
+};
+struct program_stream_pack_header
+{
+    pack_start_code PackStart;// 4
+
+    uint8_t         system_clock_reference_base21:2;
+    uint8_t         marker_bit:1;
+    uint8_t         system_clock_reference_base1:3;
+    uint8_t         fix_bit:2;    //'01'
+
+    uint8_t         system_clock_reference_base22;
+
+    uint8_t         system_clock_reference_base31:2;
+    uint8_t         marker_bit1:1;
+    uint8_t         system_clock_reference_base23:5;
+
+    uint8_t         system_clock_reference_base32;
+
+    uint8_t         system_clock_reference_extension1:2;
+    uint8_t         marker_bit2:1;
+    uint8_t         system_clock_reference_base33:5; //system_clock_reference_base 33bit
+
+    uint8_t         marker_bit3:1;
+    uint8_t         system_clock_reference_extension2:7; //system_clock_reference_extension 9bit
+
+    uint8_t         program_mux_rate1;
+
+    uint8_t         program_mux_rate2;
+
+    uint8_t         marker_bit5:1;
+    uint8_t         marker_bit4:1;
+    uint8_t         program_mux_rate3:6;
+
+    uint8_t         pack_stuffing_length:3;
+    uint8_t         reserved : 5;
+};
+struct program_system_pack_header
+{
+    pack_start_code     PackStart;// 4
+    littel_endian_size  PackLength;//2
+
+    uint32_t            marker_bit1:1;//1
+    uint32_t            rate_bound:22;
+    uint32_t            marker_bit2:1;
+    uint32_t            audio_bound:6;
+    uint32_t            fixed_flag:1;
+    uint32_t            CSPS_flag:1;
+
+    uint16_t system_audio_lock_flag:1;
+    uint16_t system_video_lock_flag:1;
+    uint16_t            marker_bit3:1;
+    uint16_t            video_bound:5;
+    uint16_t packet_rate_restriction_flag:1;
+    uint16_t            reserved_bits:7;
+
+    uint8_t             streamid;
+    uint8_t             reserved[5];
+};//18
+
+struct program_elementary_stream_info
+{
+    uint8_t             stream_type;
+    uint8_t             elementary_stream_id;
+    uint8_t             elementary_stream_info_length[2];
+};
+struct program_elementary_stream_map_info
+{
+    uint8_t             elementary_stream_map_length[2];
+};
+
+
+struct program_stream_map
+{
+    pack_start_code    PackStart;
+    littel_endian_size PackLength;//we mast do exchange
+
+    uint8_t            program_stream_map_version:5;
+    uint8_t            reserved1:2;
+    uint8_t            current_next_indicator:1;
+
+    uint8_t            marker_bit:1;
+    uint8_t            reserved2:7;
+
+    uint8_t            program_stream_info_length[2];
+
+};
+struct program_private_head
+{
+    pack_start_code PackStart;
+    littel_endian_size PackLength;//we mast do exchange
+};
+struct program_stream_e
+{
+    pack_start_code     PackStart;
+    littel_endian_size  PackLength;//we mast do exchange
+    char                PackInfo1[2];
+    unsigned char       stuffing_length;
+};
+
+
+#define RTP_PLAYLOAD_LEN   1400
+#define RTP_PACK_BUF_SIZE   1500
+
+#define AU_HEADER_SIZE 4
+#define AAC_EXTRA      7
+#define AU_HALF_SIZE   2
+
+struct es_frame_info
+{
+    uint8_t            payload;
+    uint32_t           length;
+    uint8_t            keyframe;
+    uint8_t            streamId;
+    uint32_t           timestamp;
+    uint32_t           ssrc;
+};
+
+#pragma pack(pop)
+
 
 
 
